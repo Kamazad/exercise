55d849bed4441810eef94a3cdc65add667b96459
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-01-27 21:07:36 +0100

    Exercise 7

--- src/main/java/Exercise_1_SDA/Ex7/Main.java
+++ src/main/java/Exercise_1_SDA/Ex7/Main.java
@@ -1,4 +1,13 @@
 package Exercise_1_SDA.Ex7;
 
 public class Main {
+    public static void main(String[] args) {
+        int lenght = 3;
+        int firstElement = 3;
+        int differnce = 4;
+
+        for (int i = 0; i < lenght; i++) {
+            System.out.println(i * differnce + firstElement);
+        }
+    }
 }
