c52f1336478d1598c6b94fb6b740b99e5c14e624
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-01-27 20:22:46 +0100

    Exercise 3

--- src/main/java/Exercise_1_SDA/Ex3/Main.java
+++ src/main/java/Exercise_1_SDA/Ex3/Main.java
@@ -1,4 +1,34 @@
 package Exercise_1_SDA.Ex3;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int x, y;
+        Scanner scannner = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę");
+        x = scannner.nextInt();
+        System.out.println("Podaj drugą liczbę");
+
+        y = scannner.nextInt();
+        Integer[] suma = new Integer[]{0};
+        if (x - y < 0) {
+
+            for (int i = x; i <= y; i++) {
+                suma[0] += i;
+            }
+            System.out.println("Suma liczb od : " + x + " do : " + y + " wynosi : " + suma[0]);
+        } else if (x - y > 0) {
+
+            for (int i = y; i <= x; i++) {
+                suma[0] += i;
+            }
+            System.out.println("Suma liczb od : " + x + " do : " + y + " wynosi : " + suma[0]);
+        } else {
+            System.out.println("podane liczby są takie same");
+        }
+
+
+
+    }
 }
