90b28e2ac68794f1df27fd55aab8be200f1c959a
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-01-27 20:26:29 +0100

    Exercise 4 from _1_SDA

--- src/main/java/Exercise_1_SDA/Ex4/Main.java
+++ src/main/java/Exercise_1_SDA/Ex4/Main.java
@@ -1,4 +1,13 @@
 package Exercise_1_SDA.Ex4;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] tablica = new int[]{12, 7, 19};
+        int tmp = tablica[0];
+        tablica[0] = tablica[2];
+        tablica[2] = tmp;
+        for (int printTable : tablica) {
+            System.out.print(printTable + " ");
+        }
+    }
 }
