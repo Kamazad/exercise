7b8f3b369999df17a68f5b28dfac923ca1eaf4c5
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-01-31 22:34:42 +0100

    Exercise 4 from  1_MZ

--- src/main/java/Exercise_1_MZ/Ex4/Main.java
+++ src/main/java/Exercise_1_MZ/Ex4/Main.java
@@ -1,4 +1,34 @@
 package Exercise_1_MZ.Ex4;
 
+import java.util.ArrayList;
+
+/**
+ * Utwórz listę ArrayList przechowującą imiona.
+ * a. Dodaj 5 imion do listy,
+ * b. Wykorzystując pętle for pobierz i wyświetl kolejno wszystkie elementy z listy.
+ * c. Wykorzystując pętle for pobierz i wyświetl kolejno wszystkie elementy z listy
+ * zaczynając od ostatniego, a kończąc na pierwszym.
+ * */
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        //a.
+        names.add("Krzyś");
+        names.add("Joanna");
+        names.add("Tadeusz");
+        names.add("Staś");
+        names.add("Helenka");
+
+        //b.
+        for (int i = 0; i< names.size(); i++){
+            System.out.println(names.get(i));
+        }
+
+        //c.
+        for (int i = names.size()-1; i>= 0; i--){
+            System.out.println(names.get(i));
+        }
+
+    }
 }
