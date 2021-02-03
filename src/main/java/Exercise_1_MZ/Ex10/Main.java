14ba21628b0752e64a5a9c67c2a874026eb32d79
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-02-03 23:34:04 +0100

    Exercise 10 & 11

--- src/main/java/Exercise_1_MZ/Ex10/Main.java
+++ src/main/java/Exercise_1_MZ/Ex10/Main.java
@@ -1,8 +1,14 @@
 package Exercise_1_MZ.Ex10;
 
+import java.time.LocalDate;
+
 /**
  * 10. Wykorzystując obiekt typu LocalDate wyświetl aktualną datę.
  * */
 
 public class Main {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+    }
 }
--- src/main/java/Exercise_1_MZ/Ex11/Main.java
+++ src/main/java/Exercise_1_MZ/Ex11/Main.java
@@ -1,8 +1,14 @@
 package Exercise_1_MZ.Ex11;
 
+import java.time.LocalDateTime;
+
 /**
  * 11. Wykorzystując obiekt typu LocalDateTime wyświetl aktualną datę i godzinę.
  * */
 
 public class Main {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
 }
