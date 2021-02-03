d919d035cdaae3ea0c4d68928a4fc5056cd98f1a
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-02-03 23:16:44 +0100

    Exercise7_MZ

--- src/main/java/Exercise_1_MZ/Ex7/Main.java
+++ src/main/java/Exercise_1_MZ/Ex7/Main.java
@@ -1,5 +1,8 @@
 package Exercise_1_MZ.Ex7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Utwórz listę ArrayList przechowującą liczby całkowite.
  * a. Dodaj 5 liczb do listy.
@@ -10,4 +13,27 @@ package Exercise_1_MZ.Ex7;
 * */
 
 public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        //a.
+        list.add(56733);
+        list.add(254724);
+        list.add(54564);
+        list.add(32234);
+        list.add(92113);
+
+        //b.
+        //IndexOutOfBoundException
+        //list.get(6);
+
+        //c.
+
+        //d.
+        try {
+            list.get(6);
+        } catch (IndexOutOfBoundsException ioobe){
+            System.err.println("Pobrałeś wartość spoza zakresu tablicy " + ioobe.getMessage());
+        }
+    }
 }
