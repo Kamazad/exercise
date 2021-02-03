a12ea780c33e505c0025f1c4dac4a862348a7fda
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-02-03 23:32:49 +0100

    Exercise 9

--- src/main/java/Exercise_1_MZ/Ex9/Main.java
+++ src/main/java/Exercise_1_MZ/Ex9/Main.java
@@ -1,8 +1,16 @@
 package Exercise_1_MZ.Ex9;
 
+
+import java.time.LocalTime;
+
 /**
  * 9. Wykorzystując obiekt typu LocalTime wyświetl aktualny czas.
  * */
 
 public class Main {
+    public static void main(String[] args) {
+
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+    }
 }
