fba521dd5ef011760e3bae739cb4f577a0f81e81
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-02-03 23:37:07 +0100

    Exercise 12

--- src/main/java/Exercise_1_MZ/Ex12/Main.java
+++ src/main/java/Exercise_1_MZ/Ex12/Main.java
@@ -1,5 +1,8 @@
 package Exercise_1_MZ.Ex12;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 /**
  * 12. Utwórz obiekt typu LocalDate przechowujący datę 01.01.2017 oraz obiekt typu
  * LocalDate przechowujący datę 05.05.2017. Wykorzystując obiekt typu Period
@@ -7,4 +10,12 @@ package Exercise_1_MZ.Ex12;
 */
 
 public class Main {
+    public static void main(String[] args) {
+        LocalDate firstDate = LocalDate.of(2017,1,1);
+        LocalDate secondDate = LocalDate.of(2017,5,5);
+
+        Period period = Period.between(firstDate,secondDate);
+
+        System.out.println(period);
+    }
 }
