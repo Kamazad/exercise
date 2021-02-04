e3d4b231d33566ad2cc08f6c105d9caf9f515b83
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2021-02-04 21:49:52 +0100

    Exercise 14

--- src/main/java/Exercise_1_MZ/Ex14/Main.java
+++ src/main/java/Exercise_1_MZ/Ex14/Main.java
@@ -1,7 +1,16 @@
 package Exercise_1_MZ.Ex14;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 /**
  * 14. Wyświetl bieżącą datę i godzinę w Tokyo.
  */
 public class Main {
+    public static void main(String[] args) {
+        ZoneId zoneId = ZoneId.of("Asia/Tokyo");
+        LocalDateTime localDateTime = LocalDateTime.now(zoneId);
+
+        System.out.println(localDateTime);
+    }
 }
